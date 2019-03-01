c937669c2452904d3efee4912b52513d2854af91
Reiss, Gerd <Gerd.Reiss@finnova.com>
2019-03-01 14:07:57 +0100

    finished?

--- src/Gitup.hs
+++ src/Gitup.hs
@@ -2,13 +2,15 @@ module Gitup
   ( updateAll
   ) where
 
-import Control.Monad
-import System.Directory
-import System.FilePath
+import Control.Monad (filterM)
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath (FilePath, (</>))
+import System.Process.Typed (proc, runProcess_)
 
 updateAll :: String -> IO ()
 updateAll dir =
-  listSubdirs dir >>= filterM doesDirectoryExist >>= filterM isGitRepo >>= print
+  listSubdirs dir >>= filterM doesDirectoryExist >>= filterM isGitRepo >>=
+  mapM_ updateRepo
 
 listSubdirs :: FilePath -> IO [FilePath]
 listSubdirs root =
@@ -16,3 +18,9 @@ listSubdirs root =
 
 isGitRepo :: FilePath -> IO Bool
 isGitRepo dir = elem ".git" <$> listDirectory dir
+
+updateRepo :: FilePath -> IO ()
+updateRepo path = do
+  runProcess_ $ proc "cd" [path]
+  putStrLn $ "updating " ++ path
+  runProcess_ $ proc "git" ["pull"]
